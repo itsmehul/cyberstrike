98fb09fdb6a1f9999cf24a25a3e82c121e74adfc
mrgawde <mehulgawdenxt@gmail.com>
2018-08-31 01:33:44 +0530

    feat: Smart navigation bar

--- src/components/header.js
+++ src/components/header.js
@@ -3,43 +3,87 @@ import Link from 'gatsby-link'
 import Headroom from 'react-headroom'
 import { withPrefix } from 'gatsby-link'
 
-const Header = ({ siteTitle }) => (
-  <Headroom
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+const Header = ({ siteTitle }) => {
+  const isHomepage = location.pathname === withPrefix('/')
+  if (!isHomepage) {
+    return (
+      <Headroom
+        style={{
+          background: 'rgb(3, 21, 51)',
+          marginBottom: '1.45rem',
+        }}
+      >
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
-            marginRight: '20px',
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
           }}
         >
-          Home
-        </Link>
-        <Link
-          to="/events"
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+                marginRight: '20px',
+              }}
+            >
+              Home
+            </Link>
+            <Link
+              to="/events"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              Events
+            </Link>
+          </h1>
+        </div>
+      </Headroom>
+    )
+  } else {
+    return (
+      <Headroom
+        style={{
+          background: 'transparent',
+          marginBottom: '1.45rem',
+        }}
+      >
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
           }}
         >
-          Events
-        </Link>
-      </h1>
-    </div>
-  </Headroom>
-)
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+                marginRight: '20px',
+              }}
+            >
+              Home
+            </Link>
+            <Link
+              to="/events"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              Events
+            </Link>
+          </h1>
+        </div>
+      </Headroom>
+    )
+  }
+}
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,39 +1,32 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { withPrefix } from "gatsby-link";
 import Header from '../components/header'
-import styles from './index.module.css'
 
-const Layout = ({ children, location, data }) => {
-  const isHomepage = location.pathname === withPrefix("/");
-  if(isHomepage){
-    return(<div className={styles.splash} />)
-  }else{
-    return (
-      <div>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children()}
-        </div>
+const Layout = ({ children, data }) => {
+  return (
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: '0 auto',
+          marginTop:-5,
+          maxWidth: 960,
+          padding: '0px',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
       </div>
-    )
-  }
-
+    </div>
+  )
 }
 
 Layout.propTypes = {
