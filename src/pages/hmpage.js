3317ec87b704a267f12deebec6df9b6a6b55ec68
As-SamirK <isamkushwaha@gmail.com>
2018-08-30 23:52:00 +0530

    just another commit

--- src/pages/hmpage.js
+++ src/pages/hmpage.js
@@ -3,12 +3,9 @@ import Link from 'gatsby-link'
 
 const hmPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p>This is a cfdf </p>
-
-    <h1>Index</h1>
+    <h1>CYBERSTRIKE</h1>
+    <br/>
+    <h2>5th AND 6th DECEMBER 2018</h2>
     
   </div>
 )
