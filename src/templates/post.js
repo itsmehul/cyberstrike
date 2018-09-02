38e3090110342e9faf3111411d013ab256583d17
mrgawde <mehulgawdenxt@gmail.com>
2018-09-02 19:16:30 +0530

    refactor: Temporary changes

--- src/templates/post.js
+++ src/templates/post.js
@@ -23,18 +23,18 @@ export default function Template({ data }) {
   )
   console.log(head[0].name)
   return (
-    <div style={{ backgroundColor: 'white' }}>
+    <div>
       <div style={{height:'290px', width:'100%', overflow:'hidden'}}>
         <img src={widescreen} style={{height:'auto',width:'100%', borderRadius:'10px', boxShadow: '0 2px 5px rgba(0, 0, 4, 0.6)'}}/>
       </div>
       <h1 className={styles.maineventhead}>{title}</h1>
       <h3 className={styles.maineventdescription}>{short}</h3>
-      <p>{fee}</p>
+      {/* <p>{fee}</p>
       <p>{teamSize}</p>
-      <p>{Type}</p>
+  <p>{Type}</p>*/}
       <p className={styles.maineventdate}>{Date}</p>
-      <p>Invigilator</p>
-        <ContactCard name={head[0].name} head={head[0].head} contact={head[0].contact}/>
+      {/*<p>Invigilator</p>
+        <ContactCard name={head[0].name} head={head[0].head} contact={head[0].contact}/> */}
     </div>
   )
 }
