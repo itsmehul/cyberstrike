1b6891c6bed40decbfccbc16f63a8cf6de67f2f2
mrgawde <mehulgawdenxt@gmail.com>
2018-09-02 10:06:53 +0530

    refactor: Clean up files

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import Contact from '../components/contacts'
 import contactsData from '../data/contacts.json'
 import styles from './contacts.module.css'
 
 const contactUs = ({ data }) => {
   document.body.style.background = "white";
-
-  console.log(Object.values(contactsData))
   return (
     <div> 
       <h1 className={styles.contacthead}>Happy to Help!</h1>
@@ -27,22 +24,5 @@ const contactUs = ({ data }) => {
     </div>
   )
 }
-export const pageQuery = graphql`
-  query contactUs {
-    markdownRemark(frontmatter: { path: { eq: "/contacts" } }) {
-      html
-      frontmatter {
-        path
-        title
-        src
-        short
-        fee
-        teamSize
-        Type
-        Date
-      }
-    }
-  }
-`
 
 export default contactUs
--- src/pages/events.js
+++ src/pages/events.js
@@ -2,52 +2,45 @@ import React from 'react'
 import { withPrefix } from 'gatsby-link'
 import styles from './events.module.css'
 import Link from 'gatsby-link'
-import { Heading, Image, Paragraph, Card } from 'reakit'
+import { Heading, Image, Card } from 'reakit'
 
 const EventsPage = ({ data }) => {
-  document.body.style.background = "url(https://image.ibb.co/hQk0QK/fondo_web_02.jpg)";
-  
+  document.body.style.background =
+    'url(https://image.ibb.co/hQk0QK/fondo_web_02.jpg)'
+
   return (
     <div>
       <div className={styles.eventhead}>GAMES & EVENTS</div>
-    <div className={styles.grid}>
-      {data.allMarkdownRemark.edges.map(post => {
-        const {
-          src,
-          title,
-          short,
-          fee,
-          teamSize,
-          type,
-          path,
-        } = post.node.frontmatter
-        return (
-          
-          
-          <div>
-            <div className={styles.eventCards}>
-              <Heading
-              className={styles.title}
-              >{title}</Heading>
-              <Card.Fit
-                as={Image}
-                src={src}
-                alt="Kitten"
-                width="100%"
-                height="auto"
-              />
+      <div className={styles.grid}>
+        {data.allMarkdownRemark.edges.map(post => {
+          const {
+            src,
+            title,
+            short,
+            path
+          } = post.node.frontmatter
+          return (
+            <div>
+              <div className={styles.eventCards}>
+                <h1 className={styles.title}>{title}</h1>
+                <div
+                  style={{ height: '200px', width: '100%', overflow: 'hidden' }}
+                >
+                  <img src={src} style={{ height: 'auto', width: '100%' }} />
+                </div>
 
-              <p className={styles.eventpara}>{short}</p> 
-              
-              <div className={styles.eventlinkdiv}>
-                <Link className={styles.eventlink} to={path}>MORE INFO</Link>
+                <p className={styles.eventpara}>{short}</p>
+
+                <div className={styles.eventlinkdiv}>
+                  <Link className={styles.eventlink} to={path}>
+                    MORE INFO
+                  </Link>
+                </div>
               </div>
-              
             </div>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
