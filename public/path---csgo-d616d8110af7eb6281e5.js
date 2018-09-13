162f4dbbd17a5082561b200e168438e794e24dcd
mrgawde <mehulgawdenxt@gmail.com>
2018-09-13 23:48:30 +0530

    tseet

---
+++ public/path---csgo-d616d8110af7eb6281e5.js
@@ -0,0 +1,2 @@
+webpackJsonp([27672113942254],{276:function(e,t){e.exports={data:{markdownRemark:{html:"",frontmatter:{path:"/csgo",title:"Counter Strike",short:"The most loved games of all times- challenge your friends to win the tournament and challenge your rivals to earn the title!",fee:"₹250 Per Team",teamSize:"5",Type:"Tournament",Date:"December 5, 2018 & December 6, 2018",widescreen:"static/assets/cover/csgo.jpg",game:"csgo"}}},pathContext:{}}}});
+//# sourceMappingURL=path---csgo-d616d8110af7eb6281e5.js.map
